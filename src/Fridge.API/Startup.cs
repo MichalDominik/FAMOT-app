d97c65f75ba009987246142f3d53320dcfae958e
Michal_Dominik <priv@michaldominik.pl>
2021-09-26 12:44:19 +0200

    database creation

--- src/Fridge.API/Startup.cs
+++ src/Fridge.API/Startup.cs
@@ -1,3 +1,4 @@
+using Fridge.Repository.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fridge.API
 {
@@ -24,6 +26,10 @@ namespace Fridge.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FridgeDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("MyConnection"));
+            });
             services.AddControllers();
         }
 
