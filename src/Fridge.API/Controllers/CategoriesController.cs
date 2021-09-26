fa4d63a1926e663e3bd229e2554183083abd869c
Michal_Dominik <priv@michaldominik.pl>
2021-09-26 14:00:28 +0200

    controllers

---
+++ src/Fridge.API/Controllers/CategoriesController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Fridge.API.Dtos.Category;
+using Fridge.Service.Interfaces;
+using Fridge.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fridge.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : MainController
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(IMapper mapper, ICategoryService categoryService)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryAddDto categoryDto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var category = _mapper.Map<Category>(categoryDto);
+            var categoryResult = await _categoryService.Add(category);
+
+            if (categoryResult == null) return BadRequest();
+
+            return Ok(_mapper.Map<CategoryResultDto>(categoryResult));
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, CategoryEditDto categoryDto)
+        {
+            if (id != categoryDto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            await _categoryService.Update(_mapper.Map<Category>(categoryDto));
+
+            return Ok(categoryDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var categories = await _categoryService.GetAll();
+            Category categoryToRemove = null;
+            foreach (var category in categories)
+            {
+                if (category.Id == id)
+                {
+                    categoryToRemove = category;
+                }
+            }
+            if (categoryToRemove == null) return NotFound();
+
+            var result = await _categoryService.Remove(categoryToRemove);
+
+            if (!result) return BadRequest();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAll();
+
+            if (categories == null) return NotFound();
+
+            return Ok(_mapper.Map<IEnumerable<CategoryResultDto>>(categories));
+        }
+
+        [Route("search/{category}")]
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> Search(string category)
+        {
+            var categories = _mapper.Map<List<Category>>(await _categoryService.Search(category));
+
+            if (categories == null || categories.Count == 0) return NotFound();
+
+            return Ok(categories);
+        }
+    }
+}
---
+++ src/Fridge.API/Controllers/ProductsController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Fridge.API.Dtos.Product;
+using Fridge.Service.Interfaces;
+using Fridge.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fridge.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : MainController
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IMapper mapper, IProductService productService)
+        {
+            _mapper = mapper;
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductAddDto productDto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var product = _mapper.Map<Product>(productDto);
+            var productResult = await _productService.Add(product);
+
+            if (productResult == null) return BadRequest();
+
+            return Ok(_mapper.Map<ProductResultDto>(productResult));
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, ProductEditDto productDto)
+        {
+            if (id != productDto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            await _productService.Update(_mapper.Map<Product>(productDto));
+
+            return Ok(productDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var products = await _productService.GetAll();
+            Product productToRemove = null;
+            foreach (var product in products)
+            {
+                if (product.Id == id)
+                {
+                    productToRemove = product;
+                }
+            }
+            if (productToRemove == null) return NotFound();
+
+            var result = await _productService.Remove(productToRemove);
+
+            if (!result) return BadRequest();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAll();
+
+            if (products == null) return NotFound();
+
+            return Ok(_mapper.Map<IEnumerable<ProductResultDto>>(products));
+        }
+
+        [Route("search/{productName}")]
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Search(string productName)
+        {
+            var products = _mapper.Map<List<Product>>(await _productService.Search(productName));
+
+            if (products == null || products.Count == 0) return NotFound();
+
+            return Ok(products);
+        }
+    }
+}
